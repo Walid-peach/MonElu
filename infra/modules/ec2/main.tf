656627466e0b7f953cba3007516ef01a94c0a3c9
Walid-peach <walidelkhoukh99@gmail.com>
2026-06-01 22:50:17 +0200

    feat: attach AmazonSSMManagedInstanceCore policy to EC2 IAM role

--- infra/modules/ec2/main.tf
+++ infra/modules/ec2/main.tf
@@ -59,6 +59,11 @@ resource "aws_iam_role_policy" "s3_access" {
   })
 }
 
+resource "aws_iam_role_policy_attachment" "ssm" {
+  role       = aws_iam_role.ec2.name
+  policy_arn = "arn:aws:iam::aws:policy/AmazonSSMManagedInstanceCore"
+}
+
 resource "aws_iam_instance_profile" "ec2" {
   name = "${var.name_prefix}-ec2-profile"
   role = aws_iam_role.ec2.name
