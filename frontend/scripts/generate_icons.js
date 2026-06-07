02c9779a2674444ea779660dfca28b9dfce0a660
Walid-peach <walidelkhoukh99@gmail.com>
2026-06-07 19:07:24 +0200

    feat: add Geist fonts and icon generation script to assets

---
+++ frontend/scripts/generate_icons.js
@@ -0,0 +1,5 @@
+const fs = require('fs')
+// If canvas not available, just create placeholder PNGs
+// We'll use the SVG logo from the existing landing page
+console.log('Place icon-192.png and icon-512.png in public/')
+console.log('Use the MonElu hemicycle SVG from the landing page, export at those sizes')
