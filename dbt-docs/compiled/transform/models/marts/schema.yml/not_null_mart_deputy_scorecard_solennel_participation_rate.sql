8a6949f9858d635d39b78688ba84858f4ceb7b95
Walid-peach <Walid-peach@users.noreply.github.com>
2026-07-10 10:48:08 +0000

    deploy: d2cd6b58b1e23e565e3b403d72a6d277aa2c850e

---
+++ dbt-docs/compiled/transform/models/marts/schema.yml/not_null_mart_deputy_scorecard_solennel_participation_rate.sql
@@ -0,0 +1,11 @@
+
+    
+    
+
+
+
+select solennel_participation_rate
+from "postgres"."analytics_marts"."mart_deputy_scorecard"
+where solennel_participation_rate is null
+
+
---
+++ dbt-docs/compiled/transform/models/marts/schema.yml/not_null_mart_deputy_scorecard_voting_days_rate.sql
@@ -0,0 +1,11 @@
+
+    
+    
+
+
+
+select voting_days_rate
+from "postgres"."analytics_marts"."mart_deputy_scorecard"
+where voting_days_rate is null
+
+
